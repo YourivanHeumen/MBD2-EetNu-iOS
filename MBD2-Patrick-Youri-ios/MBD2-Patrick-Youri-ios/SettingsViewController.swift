034e31237498fc2d9c1388d4bd5d9ca39ed22a1f
PatrickVanOverdijk <user@Users-Mac.local>
2015-04-11 12:29:37 -0700

    Settings opslaan (maxDistance)

--- MBD2-Patrick-Youri-ios/MBD2-Patrick-Youri-ios/SettingsViewController.swift
+++ MBD2-Patrick-Youri-ios/MBD2-Patrick-Youri-ios/SettingsViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        var maxDistance: Int? = userDefaults.objectForKey("maxDistance") as Int?
+        if (maxDistance == nil)
+        {
+            maxDistance = 10
+            userDefaults.setObject(maxDistance, forKey: "maxDistance")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBOutlet weak var Slider: UISlider!
     @IBOutlet weak var Distance: UILabel!
+    
     @IBAction func sliderValueChanged(sender: UISlider) {
         var distanceValue = Int(sender.value)
         Distance.text = "\(distanceValue) Km"
+        userDefaults.setObject(distanceValue, forKey: "maxDistance")
     }
+    
+    
 }
