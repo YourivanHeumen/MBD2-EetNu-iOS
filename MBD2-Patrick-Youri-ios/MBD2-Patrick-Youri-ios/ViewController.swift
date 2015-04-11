27e948b8b7a3134616eeca173829ebab9bd4a022
PatrickVanOverdijk <user@Users-Mac.local>
2015-04-11 08:48:32 -0700

    Initial Nav bar and tab bar for main view and settings screen

---
+++ MBD2-Patrick-Youri-ios/MBD2-Patrick-Youri-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MBD2-Patrick-Youri-ios
+//
+//  Created by User on 11/04/15.
+//  Copyright (c) 2015 Patrick-Youri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MBD2-Patrick-Youri-ios/MBD2-Patrick-Youri-iosTests/MBD2_Patrick_Youri_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  MBD2_Patrick_Youri_iosTests.swift
+//  MBD2-Patrick-Youri-iosTests
+//
+//  Created by User on 11/04/15.
+//  Copyright (c) 2015 Patrick-Youri. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MBD2_Patrick_Youri_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
